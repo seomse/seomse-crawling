94db034f55cc3b38a02ff4b537c92fdcabe83b59
macle <ysys86a@gmail.com>
2019-11-14 21:57:13 +0900

    크롤링 버그패치

--- src/main/java/com/seomse/crawling/ha/ActiveAddrPortApi.java
+++ src/main/java/com/seomse/crawling/ha/ActiveAddrPortApi.java
@@ -1,17 +1,17 @@
 package com.seomse.crawling.ha;
 
 import com.seomse.api.ApiMessage;
-import com.seomse.commons.communication.HostAddrPort;
 import com.seomse.commons.utils.ExceptionUtil;
 import com.seomse.system.commons.CommonConfigs;
 import com.seomse.system.engine.console.EngineConsole;
+import com.seomse.system.server.console.ServerConsole;
 
 /**
  * <pre>
  *  파 일 명 : ActiveAddrPortApi.java
  *  설    명 : 엑티브 서버의 접속정보 얻기
  *  작 성 자 : macle
- *  작 성 일 : 2019.11.11
+ *  작 성 일 : 2019.11.14
  *  버    전 : 1.0
  *  수정이력 :
  *  기타사항 :
@@ -24,9 +24,14 @@ public class ActiveAddrPortApi extends ApiMessage {
     public void receive(String message) {
         try {
             String crawlingEngineId = CommonConfigs.getConfig(CrawlingHighAvailabilityKey.ACTIVE_ENGINE_ID);
-            HostAddrPort addrPort = EngineConsole.getHostAddrPort(crawlingEngineId);
+            String serverId = EngineConsole.getServerId(crawlingEngineId);
+            String hostAddress = ServerConsole.getHostAddressOut(serverId);
 
-            sendMessage("S" + addrPort.getHostAddress() + "," + addrPort.getPort());
+            String port = EngineConsole.getEngineConfig(crawlingEngineId,"crawling.server.port.out");
+            if(port == null){
+                port = EngineConsole.getEngineConfig(crawlingEngineId,"crawling.server.port");
+            }
+            sendMessage("S" + hostAddress + "," + port);
         }catch(Exception e){
             sendMessage("F" + ExceptionUtil.getStackTrace(e));
         }
