3a5c16b7950cbe1e3376054f60cf9e35cfc852bc
macle <ysys86a@gmail.com>
2020-03-27 13:44:05 +0900

    dependencies update

--- src/main/java/com/seomse/crawling/ha/CrawlingActive.java
+++ src/main/java/com/seomse/crawling/ha/CrawlingActive.java
@@ -19,11 +19,11 @@ import java.util.List;
  *  설    명 : 크롤링 엑티브 역할 수행
  *  작 성 자 : macle
  *  작 성 일 : 2019.11.11
- *  버    전 : 1.0
- *  수정이력 :
+ *  버    전 : 1.1
+ *  수정이력 : 2020.03.27
  *  기타사항 :
  * </pre>
- * @author Copyrights 2019 by ㈜섬세한사람들. All right reserved.
+ * @author Copyrights 2019 ~ 2020 by ㈜섬세한사람들. All right reserved.
  */
 public class CrawlingActive {
 
@@ -43,24 +43,29 @@ public class CrawlingActive {
             }
         };
 
+        String packagesValue = Config.getConfig(CrawlingHighAvailabilityKey.INITIALIZER_PACKAGE, "com.seomse");
 
-        String initializerPackage = Config.getConfig(CrawlingHighAvailabilityKey.INITIALIZER_PACKAGE, "com.seomse");
-
-        Reflections ref = new Reflections (initializerPackage);
+        String [] initPackages = packagesValue.split(",");
         List<CrawlingActiveInitializer> initializerList = new ArrayList<>();
-        for (Class<?> cl : ref.getSubTypesOf(CrawlingActiveInitializer.class)) {
-            try{
-                CrawlingActiveInitializer initializer = (CrawlingActiveInitializer)cl.newInstance();
-                initializerList.add(initializer);
-
-            }catch(Exception e){logger.error(ExceptionUtil.getStackTrace(e));}
+        for(String initPackage : initPackages) {
+            Reflections ref = new Reflections(initPackage);
+            for (Class<?> cl : ref.getSubTypesOf(CrawlingActiveInitializer.class)) {
+                try {
+                    CrawlingActiveInitializer initializer = (CrawlingActiveInitializer) cl.newInstance();
+                    initializerList.add(initializer);
+                } catch (Exception e) {
+                    logger.error(ExceptionUtil.getStackTrace(e));
+                }
+            }
         }
-
         if(initializerList.size() == 0){
             logger.debug("crawling active start");
             return;
         }
+
         CrawlingActiveInitializer[] initializerArray = initializerList.toArray(new CrawlingActiveInitializer[0]);
+        initializerList.clear();
+
         Arrays.sort(initializerArray, PriorityUtil.PRIORITY_SORT);
         //순서 정보가 꼭맞아야하는 정보라 fori 구문 사용 확실한 인지를위해
         //noinspection ForLoopReplaceableByForEach
@@ -69,11 +74,8 @@ public class CrawlingActive {
                 initializerArray[i].init();
             }catch(Exception e){logger.error(ExceptionUtil.getStackTrace(e));}
         }
+
         logger.debug("crawling active start");
     }
-
-
-
-
 
 }
